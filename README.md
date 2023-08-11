5d9c99ba72949ae51fa1592cbcee0a7a35acc246
Douglas Maringa <douglasmaringa4@gmail.com>
2023-08-11 13:27:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Personal portfolio
+
+## Install & run
+
+Make sure you have nodejs `18.0.0` or higher and npm `8.6.0` or higher installed. Install dependencies with:
+
+```bash
+npm install
+```
+
+Once it's done start up a local server with:
+
+```bash
+npm run dev
+```
+
+To view the components storybook:
+
+```bash
+npm run storybook
+```
+
+To create a production build:
+
+```bash
+npm run build
+```
+
+## Deployment
+
+```bash
+npm run deploy
+```
+
+Deploy serverless functions:
+
+```bash
+cd functions
+```
+
+```bash
+npm run deploy:api
+```
---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/douglasmaringa',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@douglasmaringa',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/douglasmaringa',
+    icon: 'github',
+  },
+];
